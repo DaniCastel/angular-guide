34583119e83cdc6e02f6d098618aae8adeaa205a
Daniela Castellanos <ldcastellanosa@gmail.com>
2021-04-05 20:29:21 -0500

    data binding,directives and components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss'],
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = this.serverName + 'Server was created.';
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
