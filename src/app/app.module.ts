a5f287e8991133772bcb96a473f6822d8cbc62ad
Daniela Castellanos <ldcastellanosa@gmail.com>
2021-04-13 18:34:19 -0500

    first components and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms'; 
-
-
+import { FormsModule } from '@angular/forms';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule  
-  ],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
